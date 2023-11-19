f7df74895e867105f5f694a4e546bbfe766f9629
kkb <0furiousblade0@gmail.com>
2023-11-19 10:21:03 +0900

    [#12 Fixed] Django - admin (2)

--- liongram/posts/admin.py
+++ liongram/posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post, Comment
+# 이미 posts 내부에 같은 선상에 있기 때문에 .models로 사용
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+
--- liongram/posts/models.py
+++ liongram/posts/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     content = models.TextField(verbose_name='내용')
-    created_at = models.DateTimeField(verbose_name='작성일')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
     post = models.ForeignKey(to='Post', on_delete=models.CASCADE)
     # 처음은 to, 두번째는 on_delete(게시글이 삭제됐을 때 댓글도 삭제되는 기능)
     writer = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, blank=True)
