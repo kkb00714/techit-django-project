d40a14d3d52b553c200537c3d0bc431d4f113dd3
kkb <0furiousblade0@gmail.com>
2023-11-21 17:47:28 +0900

    [#17 Update] Django - View (4)

--- liongram/config/urls.py
+++ liongram/config/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from posts.views import url_view, url_parameter_view, function_view
+from posts.views import url_view, url_parameter_view, function_view, class_view, function_list_view 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('url/', url_view),
     path('url/<str:username>/', url_parameter_view),
     # username과 같은 변수명을 가져올 수도 있음
-    path('fbv/', function_view)
+    path('fbv/', function_view),
+    path('fbv/list', function_list_view),
+    path('cbv/', class_view.as_view()),
+    # class 이기 때문에, .as_view() 를 붙여줘야 함.
+    # function 일 때는 그냥 사용해도 ok.
+    
 ]
--- liongram/posts/views.py
+++ liongram/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse, JsonResponse
+from django.views.generic.list import ListView
+
+from .models import Post
 
 def url_view(request): # request는 무조건 있어야 함
     data = {'code':'001', 'ln':'good'}
@@ -25,3 +27,12 @@ def function_view(request):
             print(f'request.POST: {request.POST}')
     return render(request, 'view.html')
     
+# 클래스 기반 뷰 
+
+class class_view(ListView):
+    model = Post
+    template_name = 'cbv_view.html'
+    
+def function_list_view(request):
+    object_list = Post.objects.all().order_by('-id')
+    return render(request, 'cbv_view.html', {'object_list':object_list})
