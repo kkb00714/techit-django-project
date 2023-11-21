74dbd04102070407566536b4a83f2d0ea14f5663
kkb <0furiousblade0@gmail.com>
2023-11-21 17:10:51 +0900

    [#17 Update] Django - View (3)

--- liongram/config/urls.py
+++ liongram/config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from posts.views import url_view, url_parameter_view
+from posts.views import url_view, url_parameter_view, function_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('url/', url_view),
-    path('url/<str:username>/', url_parameter_view)
+    path('url/<str:username>/', url_parameter_view),
     # username과 같은 변수명을 가져올 수도 있음
+    path('fbv/', function_view)
 ]
--- liongram/posts/views.py
+++ liongram/posts/views.py
@@ -9,9 +9,19 @@ def url_view(request): # request는 무조건 있어야 함
     
 def url_parameter_view(request, username):
     print('url_parameter_view()')
-    print(username)
-    print(request.GET)
+    print(f'username: {username}')
+    print(f'request.GET: {request.GET}')
     # 터미널 창에 요청을 받아와서 출력
     # ?key=value 형식으로 사용해서 확인 가능
 
     return HttpResponse(username)
+
+def function_view(request):
+
+    print(f'request.method: {request.method}')
+    if request.method == 'GET':
+            print(f'request.GET: {request.GET}')
+    elif request.method == 'POST':
+            print(f'request.POST: {request.POST}')
+    return render(request, 'view.html')
+    
