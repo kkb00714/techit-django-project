f230128efd14843a06bac58769c1f789c4bbaa28
kkb <0furiousblade0@gmail.com>
2023-12-13 21:17:18 +0900

    [#38 UPDATE] Django - Signup (2)

--- liongram/accounts/views.py
+++ liongram/accounts/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
 
-from .forms import UserCreateForm
+from accounts.forms import SignUpForm #, UserCreateForm
 
 def signup_view(request):
     # GET 요청 시 HTML 응답
     if request.method == 'GET':
-        form = UserCreationForm
+        form = SignUpForm
         context = {'form' : form}
         return render(request, 'accounts/signup.html', context)
 
     # POST 요청 시 데이터 확인 후 회원 생성
     else:
-        pass
+        form = SignUpForm(request.POST)
+        
+        if SignUpForm.is_valid():
+            # 회원가입 처리
+            # username = form.cleaned_data['username']
+            # email = form.cleaned_data['email']
+            # password2 = form.cleaned_data['password2']
+            
+            instance = form.save()
+            return redirect('index')
+            
+        else:
+            # 리다이렉트
+            return redirect('accounts:signup')
--- liongram/config/settings.py
+++ liongram/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'posts',
     # 'support',
     'users',
+    'accounts',
     'debug_toolbar',
 ]
 
