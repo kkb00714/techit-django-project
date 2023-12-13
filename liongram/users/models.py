529b3920637d7da021511dbd0486eb218f18499f
kkb <0furiousblade0@gmail.com>
2023-12-13 19:05:53 +0900

    [#36 UPDATE] Django - Users (2)

--- liongram/users/models.py
+++ liongram/users/models.py
@@ -1,12 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
+
+class UserManager(DjangoUserManager):
+    def _create_user(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError('이메일은 필수 값 입니다.')
+
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        # set_password => 사용자의 비밀번호를 해싱하여 저장함
+        user.save()
+        
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+            
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(username, email, password, **extra_fields)
+    
+
 
 class User(AbstractUser):
     phone = models.CharField(verbose_name='전화번호', max_length=11)
     
-class UserInfo(models.Model):
-    phone_sub = models.CharField(verbose_name='보조 전화번호', max_length=11)
+    objects = UserManager()
     
-    user = models.ForeignKey(to='User', on_delete=models.CASCADE)
+    
+# class UserInfo(models.Model):
+#     phone_sub = models.CharField(verbose_name='보조 전화번호', max_length=11)
+    
+#     user = models.ForeignKey(to='User', on_delete=models.CASCADE)
 
     
